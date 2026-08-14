06e5c785de07d6ad0f51c7ff89e7e1001dbdd9ef
Lucas-G-Dev <lucasgabrieldev8@gmail.com>
2026-08-14 16:53:07 -0300

    Add files via upload

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,11 +1,15 @@
-#!/bin/sh
+﻿#!/bin/sh
 set -eu
 
 API_BASE="${SIMULADOR_API_BASE:-${API_BASE:-}}"
+N8N_WEBHOOK_URL="${SIMULADOR_WEBHOOK_URL:-${N8N_WEBHOOK_URL:-}}"
+APP_SCRIPT_URL="${SIMULADOR_APPS_SCRIPT_URL:-${APP_SCRIPT_URL:-${SIMULADOR_SHEET_URL:-}}}"
 
 cat >/usr/share/nginx/html/env.js <<EOF
 window.__ENV = {
-  API_BASE: "$(printf '%s' "$API_BASE" | sed 's/\\/\\\\/g; s/"/\\"/g')"
+  API_BASE: "$(printf '%s' "$API_BASE" | sed 's/\\/\\\\/g; s/"/\\"/g')",
+  N8N_WEBHOOK_URL: "$(printf '%s' "$N8N_WEBHOOK_URL" | sed 's/\\/\\\\/g; s/"/\\"/g')",
+  APP_SCRIPT_URL: "$(printf '%s' "$APP_SCRIPT_URL" | sed 's/\\/\\\\/g; s/"/\\"/g')"
 };
 EOF
 
